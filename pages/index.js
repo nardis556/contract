288b0f814831ec87ae622e1bb93ac835b9eb9171
nardis556 <nardis55five@gmail.com>
2023-11-13 16:51:59 +0000

    asdasdsadas

--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,7 @@ export default function Home() {
   };
 
   const connectMetaMask = async () => {
-    !abi && handleAbiInput(ABI);
+    !abi && handleAbiInput(JSON(ABI));
     if (!isMetaMaskAvailable) {
       quickToast("Error", "MetaMask is not available", "error");
       return;
@@ -99,7 +99,7 @@ export default function Home() {
   };
 
   const handlePrivateKeyChange = (e) => {
-    !abi && handleAbiInput(ABI);
+    !abi && handleAbiInput(JSON(ABI));
     setPrivateKey(e.target.value);
     setConnectionMethod("privateKey");
   };
